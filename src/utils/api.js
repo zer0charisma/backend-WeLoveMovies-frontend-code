3da27b16221fd4242e843e38a8309d88c94fd8cb
zer0charisma <natalie.d.rees@gmail.com>
2024-02-12 21:57:32 -0600

    update ignore

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+  process.env.REACT_APP_API_BASE_URL || "https://welovemoviesbe.onrender.com";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
